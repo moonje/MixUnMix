e414f729a342228678c394fb494bc2bd5f42059f
algermo <algermo@mail.gvsu.edu>
2015-11-15 18:02:12 -0500

    Edited delete and methods in Mix

--- src/mixUnMix/LinkList.java
+++ src/mixUnMix/LinkList.java
@@ -104,7 +104,7 @@ public class LinkList<E> {
 		if (top == null) 
 			return false;
 		
-		//case 1: delete the first node
+		//case 1: data found at top
 		if (top.getData().equals(data)) {
 			top = top.getNext();
 			return true;
@@ -112,17 +112,11 @@ public class LinkList<E> {
 		
 		Node<E> temp = top;
 		
-		while(!temp.getData().equals(data)){
+		while(temp.getNext() != null){
 			
 			//case 2: delete a node from the middle
 			if(temp.getNext().getData().equals(data)){
 				temp.setNext(temp.getNext().getNext());
-				return true;
-			}
-			
-			//case 3: delete a node from the end
-			if(temp.getNext() == null){
-				temp.setNext(null);
 				return true;
 			}
 			
@@ -131,25 +125,14 @@ public class LinkList<E> {
 		}
 		
 		
-		//case 4: multiple occurrences of the data
+		//case 3: multiple occurrences of the data
 		
 		
-		//case 5: data not found
+		//case 4: data not found
 		return false;
 		 
 	}
 
-	/*******************************************************************
-	 * 
-	 ******************************************************************/
-	public void deleteHalfWay() {
-
-		//case 0: empty list
-		
-		
-		//case 1: 
-		
-	}
 
 	/*******************************************************************
 	 * Main Method used to test the LinkList
--- src/mixUnMix/Mix.java
+++ src/mixUnMix/Mix.java
@@ -1,5 +1,10 @@
 package mixUnMix;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /***********************************************************************
  * @author Jennifer Moon
  * @author Molly Alger
@@ -10,6 +15,12 @@ public class Mix implements IMix{
 
 	/** LinkList of Characters Representing a Message (String) **/
 	private LinkList message; 
+	
+	/** A clipboard to save pieces of the message to **/
+	private String clipboard;
+	
+	/** A String of the commands given by the user **/
+	private String commands;
 	
 	/** The Initial Set of Commands **/
 	public String cmd = "Q\t\t means QUIT\n"
@@ -23,6 +34,116 @@ public class Mix implements IMix{
 			+ "p #\t\t means PASTE from the clipboard, starting at #\n"
 			+ "c & #\t\t means COPY to clipboard, starting at & and "
 			+ "ending at # (inclusive)";
+	
+	
+	/*******************************************************************
+	 * Quits the program
+	 ******************************************************************/
+	public void quit(){
+		
+		System.exit(0);
+		
+	}
+	
+	/*******************************************************************
+	 * Inserts the character c at the given position
+	 * 
+	 * @param c the character to be inserted
+	 * @param position the position to insert the given character
+	 ******************************************************************/
+	public void insert(char c, int position){
+		
+		
+	}
+	
+	/*******************************************************************
+	 * Removes the character at the given position
+	 * 
+	 * @param position the location of the character to be removed
+	 ******************************************************************/
+	public void remove(int position){
+		
+		
+	}
+	
+	/*******************************************************************
+	 * Switches the character at one position with the character at
+	 * another position
+	 * 
+	 * @param pos1 the location of one of the characters to be switched
+	 * @param pos2 the location of the other character to be switched
+	 ******************************************************************/
+	public void switchPosition(int pos1, int pos2){
+		
+		
+	}
+	
+	/*******************************************************************
+	 * Saves the commands to a text file
+	 * 
+	 * @param filename the name of the file to be saved to
+	 ******************************************************************/
+	public void save(String filename) throws Exception{
+		
+		PrintWriter out = null;
+		try {
+
+			//open and read file
+			out = new PrintWriter(new BufferedWriter(new FileWriter
+					(filename)));
+
+			//print to file
+			for (int i = 0; i < acts.size(); i++){
+
+				
+
+			}
+
+		} catch (IOException e) {
+			throw new IOException(); 
+
+		} finally {
+			//close file
+			out.close();
+		}
+		
+	}
+	
+	/*******************************************************************
+	 * Cuts the message beginning at one position and ending at another
+	 * and then saves the cut to the clipboard
+	 * 
+	 * @param pos1 the starting position of the cut
+	 * @param pos2 the ending position of the cut
+	 ******************************************************************/
+	public void cut(int pos1, int pos2){
+		
+		
+	}
+	
+	/*******************************************************************
+	 * Pastes whatever is saved to the clipboard into the message at
+	 * the given position
+	 * 
+	 * @param position the location of where to paste the contents of
+	 * 			the clipboard
+	 ******************************************************************/
+	public void paste(int position){
+		
+		
+	}
+	
+	/*******************************************************************
+	 * Copies the message from one position to another position and
+	 * saves it to the clipboard
+	 * 
+	 * @param pos1 the starting position of the copy
+	 * @param pos2 the ending position of the copy
+	 ******************************************************************/
+	public void copy(int pos1, int pos2){
+		
+		
+	}
 	
 	/*******************************************************************
 	 * 
