4ebbe02451f2e27329d99db3f5b69a41ae799666
moonje <moonje@mail.gvsu.edu>
2015-11-15 18:01:02 -0500

    Added stuff to UnMix

--- src/mixUnMix/UnMix.java
+++ src/mixUnMix/UnMix.java
@@ -1,5 +1,9 @@
 package mixUnMix;
 
+import java.io.*;
+
+import java.util.Scanner;
+
 /***********************************************************************
  * @author Jennifer Moon
  * @author Molly Alger
@@ -12,22 +16,76 @@ public class UnMix implements IUnMix{
 	private LinkList message; 
 		
 	/*******************************************************************
+	 * UnMixes the user's message used the given file
 	 * 
+	 * @param filename, the name of the file
+	 * @param userMessage, the user's message to unmix 
+	 * @return the decoded message
 	 ******************************************************************/
 	@Override
-	public String UnMixUsingFile(String filename, String userMessage) {
-		// TODO Auto-generated method stub
+	public String UnMixUsingFile(String filename, String userMessage) 
+			throws IllegalArgumentException{
+		
+		//Ensures the user is reading from a .txt file
+		if (filename.substring(filename.lastIndexOf(".") + 1).equals(
+				"txt")) {
+
+			try {
+				Scanner fileReader = new Scanner(new File(filename));
+
+
+				while (fileReader.hasNextLine()) {
+					
+					String text = fileReader.nextLine();
+					String [] part = text.split("pizza");
+					
+					if (part[0].equals("b")){
+						//call appropriate method
+						
+					} else if (part[0].equals("r")){
+						//call appropriate method
+						
+					} else if (part[0].equals("w")){
+						//call appropriate method
+						
+					} /* else if (other things) { */
+					
+					//add more relevant things here.
+					
+				}
+				
+				//return the correct string 
+				
+				fileReader.close();
+
+			} catch (FileNotFoundException e) {
+				return "WARNING! File not found!";
+			
+			} catch (Exception e) {
+				return "WARNING! Unable to load file!";
+			}
+			
+		} else {
+			return "WARNING! Only able to open .txt files!";
+		}
+
 		return null;
 	}
 	
-
-//		System.out.println("Enter in the mixed up message:");
-//		String userMessage = "";
-//		
-//		System.out.println("Enter in the file to unmix the message:");
-//		String filename = "";
-//		
-//		System.out.println("The original message was:");
-//		System.out.println(UnMixUsingFile(filename, userMessage));
-
+	/*******************************************************************
+	 * Main method used to run the UnMix program in the console
+	 ******************************************************************/
+	public static void main (String args[]){
+		UnMix unmix = new UnMix();
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter in the mixed up message:");
+		String userMessage = scanner.next();
+		
+		System.out.println("Enter in the file to unmix the message:");
+		String filename = scanner.next();
+		
+		System.out.println("The original message was:");
+		System.out.println(unmix.UnMixUsingFile(filename, userMessage));
+	}
 }
